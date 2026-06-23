b98b5fe9549a4681290b26ad9a89e00e0ae5faaa
michelle2034 <michelleenloe26@gmail.com>
2026-06-23 16:27:42 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const
-express = require('express');
+const express = require('express');
 const path = require('path');
 const app = express();
 
-// This is crucial
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-const PORT = 3000;
-app.listen(PORT, '0.0.0.0',  () => {
-  console.log(`Server live at http://44.204.222.78:3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
